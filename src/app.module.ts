bf45808b416369efee35a69f3e47c4f7b5486a32
kamai <kamai1220000@gmail.com>
2022-11-17 20:34:28 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,17 @@ import { PlaceModule } from './place/place.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
-    PlaceModule,
-    AuthModule,
     MongooseModule.forRoot(
       'mongodb+srv://mking:mking2000@cluster0.lwywdhj.mongodb.net/?retryWrites=true&w=majority',
     ),
+    PlaceModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
