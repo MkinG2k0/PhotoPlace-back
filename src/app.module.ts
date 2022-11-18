d95903f6df1c031f94f06196aad6e852560fc87c
kamai <kamai1220000@gmail.com>
2022-11-19 01:50:00 +0300

    GoogleStrategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { GoogleStrategy } from 'src/google.strategy';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlaceModule } from './place/place.module';
@@ -18,6 +19,6 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, GoogleStrategy],
 })
 export class AppModule {}
